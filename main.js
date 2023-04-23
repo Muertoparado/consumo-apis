1fbd2a9c90304b8de27a1be279484c4c1ce1975e
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-22 19:57:15 -0500

    feat(nueva api): :beers: nuevo formato otra api
    no funciona

---
+++ main.js
@@ -0,0 +1,53 @@
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+const results = document.getElementById('results');
+
+const getSuperhero = async (id) => {
+/*  
+  const corsProxy = 'https://api.allorigins.win/raw?url=';
+  const response = await fetch(`\${corsProxy}https://superheroapi.com/api/6263517990360546/${id}`,{
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  }); */
+  
+  const response = await fetch(`https://superheroapi.com/api/6263517990360546/${id}`,{
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+      
+    }
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+searchBtn.addEventListener('click', async () => {
+
+  const superhero = await getSuperhero(searchInput.data);
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+    <img src="${superhero.image.url}" alt="${superhero.name}" />
+    <h2>${superhero.name}</h2>
+    <p>${superhero.biography.publisher}</p>
+  `;
+  results.appendChild(card);
+});
+
+/* 
+  btnSiguiente.addEventListener('click', () => {
+    if(pagina < 10){
+      pagina += 1;
+      getApi();
+    }
+  });
+  
+  btnAnterior.addEventListener('click', () => {
+    if(pagina > 1){
+      pagina -= 1;
+      getApi();
+    }
+  });
+ */
+
