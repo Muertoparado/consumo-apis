3b78293964ac8e76ff4e077b05892580ea1fc4c6
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-25 00:26:13 -0500

    fix(adaptacion api pokemon): :rocket: pruebas con el formato pokeapi

--- main.js
+++ main.js
@@ -2,26 +2,24 @@ const searchBtn = document.getElementById('searchBtn');
 const searchInput = document.getElementById('searchInput');
 const results = document.getElementById('results');
 
-const getSuperhero = async (id) => {
+ const getSuperhero = async (id) => {
  
  /*  const corsProxy = 'https://api.allorigins.win/raw?url=';
   const response = await fetch(`\${corsProxy}https://superheroapi.com/api/6263517990360546/${id}`,{
     headers: {
       "Access-Control-Allow-Origin": "*"
     }
-  }); */
+  });  */
   
-  const response = await fetch(`https://superheroapi.com/api/6263517990360546/${id}`
-  ,{
-     headers: {
-      "Access-Control-Allow-Origin": "*"
-      
-    }}
-  );
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  
+  console.log(response);
   const data = await response.json();
   console.log(data);
   return data;
-}
+
+ }
+/* getSuperhero(3);  */
 
 searchBtn.addEventListener('click', async () => {
 
@@ -29,9 +27,10 @@ searchBtn.addEventListener('click', async () => {
   const card = document.createElement('div');
   card.classList.add('card');
   card.innerHTML = `
-    <img src="${data.superhero.image.url}" alt="${data.superhero.name}" />
-    <h2>${data.superhero.name}</h2>
-    <p>${data.superhero.biography.publisher}</p>
+    <h1>${superhero.name}</h1>
+    <img src="${superhero.sprites.front_default}" alt="" />
+    <h2>${superhero.types[0].type.name}</h2>
+    <p>${superhero.abilities[0].ability.name}</p>
   `;
   results.appendChild(card);
 });
@@ -52,3 +51,22 @@ searchBtn.addEventListener('click', async () => {
   });
  */
 
+
+ /*  formato poke api prueba funcional 
+ async function obtenerSuperheroe(id) {
+    // Reemplaza YOUR_API_KEY con tu clave de API real
+   
+    const url = `https://pokeapi.co/api/v2/pokemon/ditto`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error al obtener datos de la API:', error);
+    }
+  }
+  
+  obtenerSuperheroe(2); // Obtiene información sobre el superhéroe con ID 1
+  
+ */
