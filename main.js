1cbeb90c100fb7618f5ac6fb42a9b4bc47a3375c
Muertoparado <nicolas.casanova20@gmail.com>
2023-04-25 23:24:58 -0500

    feat(adaptacion api pokemon): :rocket: prueba otro formato
    formato para paginacion fallido

--- main.js
+++ main.js
@@ -4,15 +4,8 @@ const results = document.querySelector('#results');
 const btnSiguiente = document.querySelector('#btnSiguiente');
 const btnAnterior = document.querySelector('#btnAnterior');
 
+
  const getSuperhero = async (id) => {
- 
- /*  const corsProxy = 'https://api.allorigins.win/raw?url=';
-  const response = await fetch(`\${corsProxy}https://superheroapi.com/api/6263517990360546/${id}`,{
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  });  */
-  
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   
   console.log(response);
@@ -20,12 +13,10 @@ const btnAnterior = document.querySelector('#btnAnterior');
   console.log(data);
   return data;
 
- }
-/* getSuperhero(3);  */
-
+}
+ 
 searchBtn.addEventListener('click', async () => {
-
-
+  
   const superhero = await getSuperhero(searchInput.value);
   const card = document.createElement('div');
   card.classList.add('card');
@@ -36,22 +27,52 @@ searchBtn.addEventListener('click', async () => {
     <p>${superhero.abilities[0].ability.name}</p>
   `;
   results.appendChild(card);
+
 });
+/* // 1. Función para obtener la lista de Pokémon
+const getPokemonList = async (offset) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=30&offset=\${offset}`);
+  const data = await response.json();
+  return data.results;
+};
 
+// 2. Función para mostrar la información de un Pokémon en el HTML
+const displayPokemon = async (pokemon) => {
+  const pokeData = await getSuperhero(pokemon.url);
+  // Aquí puede agregar código para mostrar la información de 'pokeData' en el HTML
+  console.log(pokeData);
+};
 
+// 3. Función para manejar la paginación
+let currentPage = 0;
+
+const loadNextPage = async () => {
+  const pokemonList = await getPokemonList(currentPage * 30);
+  currentPage++;
+
+  // Llamar a la función 'displayPokemon' para cada Pokémon en la lista
+  for (const pokemon of pokemonList) {
+    await displayPokemon(pokemon);
+  }
+};
+
+// 4. Llamar a la función para cargar la primera página de Pokémon
+loadNextPage(); */
+
+/* 
   btnSiguiente.addEventListener('click', () => {
-    if(pagina < 10){
+    if(pagina < 1){
       pagina += 1;
-      getApi();
+      getSuperhero();
     }
   });
   
   btnAnterior.addEventListener('click', () => {
     if(pagina > 1){
       pagina -= 1;
-      getApi();
+      getSuperhero();
     }
-  });
+  }); */
 
 
 
